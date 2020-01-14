6c8706b7bb61412001074565f0792e6045b246d3
xiaoyingdong <xiaoyingdong@unionpay.com>
2020-01-14 14:53:01 +0800

    科技事业部-肖鹰东 第一次提交

---
+++ src/test/java/com/unionpay/tdd/ylkjsyb/ydxiao/fztest/TddFizzBuzzTest.java
@@ -0,0 +1,44 @@
+package com.unionpay.tdd.ylkjsyb.ydxiao.fztest;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.unionpay.tdd.ylkjsyb.ydxiao.fizzbuzz.GameNumber;
+
+/**
+ * 
+ * @author xiaoyingdong
+ *
+ */
+public class TddFizzBuzzTest {
+
+	
+	@Test
+	public void should_show_fizz_test() {
+		isEquals("Fizz" , 3);
+		isEquals("Fizz" , 31);
+	}
+	
+	@Test
+	public void should_show_Buzz_test() {
+		isEquals("Buzz" , 5);
+		isEquals("Buzz" , 52);
+	}
+	
+	@Test
+	public void should_show_fizzbuzz_test() {
+		isEquals("FizzBuzz" , 15);
+		isEquals("FizzBuzz" , 35);
+	}
+	
+	@Test
+	public void should_show_nor_test() {
+		isEquals("22" , 22);
+	}
+	
+	private void isEquals(String expected, int input) {
+		GameNumber gameNumber = new GameNumber(input);
+		assertEquals(expected, gameNumber.toString());
+	}
+	
+}
